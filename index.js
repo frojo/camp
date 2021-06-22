220995fb6d78a03f0aca2ab9c820c80f68a5b6cb
fran <franmorojo@gmail.com>
2021-06-22 14:07:51 -0400

    she walks

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
 import spr_sheet from "./assets/char.png";
 import spr_meta from "./assets/char.json";
 
-// import * as TWGL  from "twgl.js";
-// let m4 = TWGL.m4;
-// let v3 = TWGL.v3;
-
 import { Scene, 
         PerspectiveCamera,
         WebGLRenderer,
@@ -23,7 +19,10 @@ import { Scene,
         Color,
         DirectionalLight,
 	Vector3,
-	Fog
+  Vector2,
+	Fog,
+  Raycaster,
+  MOUSE
 } from 'three';
 
 
@@ -49,6 +48,11 @@ const renderer = new WebGLRenderer({canvas});
 console.log(OrbitControls);
 const controls = new OrbitControls(camera, renderer.domElement);
 
+controls.mouseButtons =  {
+  ORBIT : MOUSE.RIGHT
+}
+controls.enablePan = true;
+
 // controls.movementSpeed = 1000;
 // controls.domElement = renderer.domElement;
 // controls.rollSpeed = Math.PI / 24;
@@ -73,7 +77,7 @@ light.position.set(-1, 2, 4);
 scene.add(light);
 
 class Person {
-  // our pixelart person class
+  // our papermario pixelart person class
   //
   // the spritesheet is a horizontal row of sprites
   // and it comes with a json file that has info on where the animations
@@ -111,8 +115,12 @@ class Person {
     this.plane = plane;
     scene.add( plane );
 
-    // "hack" so that we can do e.g. person.position.x = x 
-    this.position = new Vector3(0, 0, 0);
+    // our internal position tracker
+    this.position = new Vector3(0, .5, 0);
+
+    this.walkTarget = new Vector3(0, .5, 0);
+    this.walkSpeed = .03;
+    this.walking = false;
 
   }
 
@@ -129,6 +137,29 @@ class Person {
     this.end_i = anim_info.to;
   }
 
+  // <target> is Vector3 world coords on the ground plane of where our person is teleported to
+  teleport(target) {
+    // the ground plane is at y=0, but our person has to be at y=.5
+    // so as not to be cut in half by the ground plane
+    target.y = .5
+    this.position.copy(target);
+  }
+
+  // <target> is Vector3 world coords on the ground plane of where our person is walking to
+  walkTo(target) {
+
+    // the ground plane is at y=0, but our person has to be at y=.5
+    // so as not to be cut in half by the ground plane
+    target.y = .5;
+    console.log('CHANGING WALK TARGET');
+    this.walkTarget.copy(target);
+
+    this.walking = true;
+
+    //console.log(this.walkTarget);
+
+  }
+
   update() {
 
     // determine which frame we're on
@@ -142,9 +173,27 @@ class Person {
     const frame_i = this.start_i + curr_frame;
 
     this.map.offset.x = frame_i / 3.0;
+
+
+    if (this.walking) {
+    // get a unit vector in the direction from our current point to 
+    // the target by subtracting and then normalizing
+      const dir = this.walkTarget.clone()
+      dir.sub(this.position);
+      dir.normalize();
+      dir.multiplyScalar(this.walkSpeed);
+      this.position.add(dir);
+
+      // if we're close enough to the target, stop walking
+      // (avoids admittedly cool vibrating glitch)
+      const dist = this.position.distanceToSquared(this.walkTarget);
+      if (dist < .001) {
+        this.walking = false;
+      }
+      
+    }
     
-    // update position
-    // "hack" so that we can do e.g. person.position.x = x 
+    // update threejs Object3D position with our internal tracker
     this.plane.position.copy(this.position);
 
   }
@@ -199,7 +248,7 @@ function renderFrame(now) {
   then = now;
 
 
-  person.update()
+  greta.update()
 
 
   renderer.render(scene, camera);
@@ -208,19 +257,20 @@ function renderFrame(now) {
 }
 
 //var char;
+var greta;
 function main() {
   var frag_shader = document.querySelector("#fragment-shader-pixel-grass").text;
-  console.log(frag_shader);
+  //console.log(frag_shader);
 
 
   // make ground plane
   const ground_geo = new PlaneGeometry( 100, 100 );
- //    const ground_mat = new MeshPhongMaterial({
- //      color: 0x00ff00,
- //      side: DoubleSide});
+  const ground_mat = new MeshPhongMaterial({
+    color: 0x3e631d,
+    side: DoubleSide});
 
-  const ground_mat = new ShaderMaterial({
-    fragmentShader: frag_shader});
+  // const ground_mat = new ShaderMaterial({
+  //   fragmentShader: frag_shader});
   // ground_mat.fog = true;
 
   ground = new Mesh( ground_geo, ground_mat);
@@ -228,13 +278,64 @@ function main() {
   scene.add( ground );
   // makeGround(scene);
   
-  person = new Person(spr_sheet, spr_meta, scene);
-  person.position.z = 6;
-  person.position.y = .5;
-  person.startAnim('walk');
+  greta = new Person(spr_sheet, spr_meta, scene);
+  greta.teleport(new Vector3(0, 0, 6));
+  greta.startAnim('walk');
 
   requestAnimationFrame(renderFrame);
 }
+
+// input
+//
+function onMouseMove(event) {
+  console.log('mouse moving');
+
+}
+
+function addDebugCube(scene, worldCoords) {
+
+  const geometry = new BoxGeometry(1, 1, 1);
+  const material = new MeshPhongMaterial( { color: 0xffff00} );
+  const cube = new Mesh( geometry, material );
+
+
+  cube.position.copy(worldCoords);
+  scene.add(cube);
+
+}
+
+function onClick(event) {
+  console.log('mouse clicked');
+
+
+  const raycaster = new Raycaster();
+  const mouse = new Vector2();
+
+  // calculate mouse position in normalized device coordinates
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+  raycaster.setFromCamera( mouse, camera );
+
+  const intersects = raycaster.intersectObject(ground);
+
+  const walkTarget = intersects[0].point;
+
+  //console.log(walkTarget);
+
+  greta.walkTo(intersects[0].point);
+  //greta.teleport(intersects[0].point);
+
+  // greta.position.copy(intersects[0].point);
+  // greta.position.y = .5;
+
+  
+
+
+}
+
+window.addEventListener('mousemove', onMouseMove, false);
+window.addEventListener('click', onClick, false);
 
 main();
 
@@ -262,251 +363,3 @@ function isWebGLAvailable() {
 
 }
 
-// const canvas = document.querySelector('canvas');
-// 
-// const gl = canvas.getContext('webgl');
-// if (!gl) {
-//   console.log('this browser/environment does not support webgl!');
-// }
-
-
-
-function createShader(gl, type, source) {
-  var shader = gl.createShader(type);
-
-  gl.shaderSource(shader, source);
-
-  gl.compileShader(shader);
-
-  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  if (success) {
-        return shader;
-  }
-  
-  // this is only if the linking/compiling doesnt work
-  console.log(gl.getShaderInfoLog(shader));
-  gl.deleteShader(shader);
-}
-
-function createProgram(gl, vertexShader, fragmentShader) {
-    var program = gl.createProgram();
-
-    gl.attachShader(program, vertexShader);
-
-    gl.attachShader(program, fragmentShader);
-
-    gl.linkProgram(program);
-
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-          return program;
-        }
-   
-    // this is only if the linking/compiling doesnt work
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
-}
-
-// // create and compile shaders into webgl context
-// var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
-// var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
-//  
-// console.log(fragmentShaderSource);
-// 
-// var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-// var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-// 
-// console.log(vertexShader);
-// console.log(fragmentShader);
-// 
-// // create program and link shaders into program
-// var program = createProgram(gl, vertexShader, fragmentShader);
-// 
-// // ============ END PROGRAM CREATION/LINKING ===============
-// // tell webgl to use the program (that's on the GPU)
-// gl.useProgram(program);
-// 
-// var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
-// 
-// 
-// 
-// // sets this uniform on the last program that we called useProgram on
-// gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-// 
-
-// thank you https://webglfundamentals.org/webgl/lessons/webgl-3d-perspective.html
-function perspective(fieldOfViewInRadians, aspect, near, far) {
-  var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
-  var rangeInv = 1.0 / (near - far);
-
-  return [
-    f / aspect, 0, 0, 0,
-    0, f, 0, 0,
-    0, 0, (near + far) * rangeInv, -1,
-    0, 0, near * far * rangeInv * 2, 0
-  ];
-}
-
-
-// gl.drawArrays(primitiveType, offset, count);
-// assume a square sprite 32x32
-// assume that we've loaded and linked the appropriate frag and vert
-// shaders into a program and into gl
-function drawSprite(sheet, i, x, y, scale) {
-
-
-  // CONSTRUCT VERTICES FOR SQUARE AND LOAD THEM INTO THE PROGRAM
-
-  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  var positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  
-  // construct array for triangle vertices
-  // two triangles that form into a square of size 32x32
-  // top-left of triangle is at (x,y) in pixel space
-  //
-
-
-  let z = -300;
-  var positions = [
-    0, 0, z, // top left
-    32, 0, z, // top right
-    0,  32, z, // bottom left 
-    0, 32, z, // bottom left
-    32, 0, z, // top right
-    32, 32, z, // bottom right
-  ];
-  
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  
-  gl.enableVertexAttribArray(positionAttributeLocation);
-
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 3;          // 2 components per iteration
-  var type = gl.FLOAT;   // the data is 32bit floats
-  var normalize = false; // don't normalize the data
-  var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0;        // start at the beginning of the buffer
-  
-  // it binds it to the current ARRAY_BUFFER (not made explicit)
-  // so now the position attrib is bound to positionBuffer (the current ARRAY_BUFFER)
-  // and now that we've done this, we can bind something esle to
-  // ARRAY_BUFFER and use it for something else
-  gl.vertexAttribPointer(
-      positionAttributeLocation, size, type, normalize, stride, offset)
-
-  // translation coords
-  var translationUniformLocation = gl.getUniformLocation(program, "u_translation");
-  
-  gl.uniform2f(translationUniformLocation, x, y);
-  
-  // rotation params (i'm not even using them bc fuck that rn lol)
-  var rotationUniformLocation = gl.getUniformLocation(program, "u_rotation");
-  
-  gl.uniform2f(rotationUniformLocation, x, y);
-  
-  // console.log(TWGL);
-  // console.log(TWGL.m3);
-  // console.log(m3);
-
-  // // pass in the transform matrix as a uniform
-  //
-  let aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  let z_near = 1;
-  let z_far = 2000;
-  let fovRadians = Math.PI / 3;
-  let transform = perspective(fovRadians, aspect, z_near, z_far);
-  transform = m4.translate(transform, v3.create(x, y, 0), 0);
-  // transform = m4.rotate(matrix, 0);
-  // transform = m4.scale(transform, 1, 1);
-
-
-  var transformLocation = gl.getUniformLocation(program, "u_transform");
-  gl.uniformMatrix4fv(transformLocation, false, transform);// Set the matrix.
-
-
-    // make a texture sampler matrix based on i
-    // rotation params (i'm not even using them bc fuck that rn lol)
-    let sheetScaleLocation = gl.getUniformLocation(program, "u_sheetScale");
-    
-    gl.uniform1f(sheetScaleLocation, 3.0);
-
-    let sheetTranslateLocation = gl.getUniformLocation(program, "u_sheetTranslate");
-    
-    gl.uniform1f(sheetTranslateLocation, i);
-
-
-  // PREP TEXTURE THAT WILL BE PAINTED ONTO THE SQUARE
-
-  var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
-
-  var texCoordBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-      0.0,  0.0,
-      1.0,  0.0,
-      0.0,  1.0,
-      0.0,  1.0,
-      1.0,  0.0,
-      1.0,  1.0]), gl.STATIC_DRAW);
-  gl.enableVertexAttribArray(texCoordLocation);
-  gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
-
-  // Create a texture.
-  var texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-
-  // Set the parameters so we can render any size image.
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-
-  // Upload the image into the texture.
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, sheet);
-
-
-  // draw the texture onto the triangle
-  var primitiveType = gl.TRIANGLES;
-  var offset = 0;
-  var count = 6;
-  gl.drawArrays(primitiveType, offset, count);
-
-}
-
-
-var img;
-var char;
-function old_main() {
-
-  console.log(TWGL.m4);
-
-  char = new Person(spr_sheet, spr_meta);
-
-  char.startAnim('walk');
-
-  requestAnimationFrame(drawFrame);
-
-}
-
-//main();
-
-// this is the main render func
-function drawFrame(now) {
-  // variable framerate
-  let delta = now - then;
-  then = now;
-
-  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-  
-  // maps clip space onto the whole canvas (and no bigger)
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  
-  gl.clearColor(0, 0, 0, 0);
-  
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  char.draw(gl);
-
-  requestAnimationFrame(drawFrame);
-}
